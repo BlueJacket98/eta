8586aa752d892392a652055a3aa4713f911ce252
wuzhizhong <819171209@qq.com>
2021-11-17 17:25:39 -0800

    generators

--- src/main/java/com/capstone/eta/util/graph/MoRGraphGenerator.java
+++ src/main/java/com/capstone/eta/util/graph/MoRGraphGenerator.java
@@ -2,7 +2,6 @@
  * A util class that generates the graph on a certain date.
  * Call generateGraph() function to generate the graph on a specific date
  * Call main() function to test
- * @author Zhizhong Wu
  */
 package com.capstone.eta.util.graph;
 import java.util.*;
--- src/main/java/com/capstone/eta/util/graph/PreRackGraphGenerator.java
+++ src/main/java/com/capstone/eta/util/graph/PreRackGraphGenerator.java
@@ -2,10 +2,8 @@
  * A util class that generates the graph on a certain date.
  * Call generateGraph() function to generate the graph on a specific date
  * Call main() function to test
- * @author Zhizhong Wu
  */
 package com.capstone.eta.util.graph;
-import java.util.*;
 import org.javatuples.*;
 
 public class PreRackGraphGenerator extends GraphGenerator {
