d53c261df2c95bb626e650c2a9ac0e8df98a8ae5
wuzhizhong <819171209@qq.com>
2021-11-27 18:55:22 -0800

    overhaul of the logic and structure

---
+++ src/main/java/com/capstone/eta/util/weight/WeightGenerator.java
@@ -0,0 +1,17 @@
+package com.capstone.eta.util.weight;
+import java.util.*;
+
+public class WeightGenerator {
+    public static void main(String[] args) {
+    
+    }
+
+
+    static public Integer getModelWeight(String queryEdgeName, String graphName, Date date) {
+        return 1;
+    }
+
+    static public Integer getHistWeight(String queryEdgeName, String graphName, Date date) {
+        return 1;
+    }
+}
